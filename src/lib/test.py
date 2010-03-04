13ab421465ead4dab7b7c064a6d722b0c9c3e046
Anup C Shan <anupcshan@gmail.com>
2010-03-04 21:31:16 +0530

    Added test.py to verify all classes.

---
+++ src/lib/test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# Will attempt to instantiate each class in the package.
+
+import pyfoursquare
+
+n = pyfoursquare.Network('root', 'v1', '919916816898', 'passwd', 'json')
+cmgr = pyfoursquare.CacheMgr()
+u = pyfoursquare.User(n, cmgr, '1')
+v = pyfoursquare.Venue(n, cmgr, '1')
+c = pyfoursquare.Checkin(n, cmgr, '1')
